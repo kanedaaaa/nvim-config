b641c2f6ab2c3b72db3e8e094b6967d412ce8e8b
kanedaaaa <samflynn19121@gmail.com>
2024-01-26 14:10:01 +0400

    init

---
+++ init.lua
@@ -0,0 +1,16 @@
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
+
+-- Handle this 
+vim.opt.tabstop = 2 
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true 
+
+vim.opt.background = "light"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
